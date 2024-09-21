689657d61e360483d58f62df622439598c2d666c
David Rodriguez <devdevbadillo@gmail.com>
2024-09-20 19:19:15 -0600

    Remove src/main/ directory from repository

--- src/main/java/com/authentication/app/domain/services/IEmailService.java
+++
@@ -1,9 +0,0 @@
-package com.authentication.app.domain.services;
-
-import jakarta.mail.MessagingException;
-
-public interface IEmailService {
-    public void sendEmailVerifyAccount(String email, String token) throws MessagingException;
-
-    public void sendEmailRecupereAccount(String email, String token) throws MessagingException;
-}
--- src/main/java/com/authentication/app/presentation/validation/exceptions/custom/UserNotEnableException.java
+++
@@ -1,7 +0,0 @@
-package com.authentication.app.presentation.validation.exceptions.custom;
-
-public class UserNotEnableException extends Exception {
-    public UserNotEnableException(String message) {
-        super(message);
-    }
-}
